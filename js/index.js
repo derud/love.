09452563ca2eb15892677e9511c2ce69be08e620
derud <122689893+derud@users.noreply.github.com>
2023-02-01 18:04:38 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+prompt("Qual o seu nome?")
+prompt("Qual o nome da pessoa?")
+var CalculadoraDoAmor = Math.random() *100;
+CalculadoraDoAmor = Math.floor(CalculadoraDoAmor)+1;
+console.log(CalculadoraDoAmor);
+alert("A probabilidade de amor é "+ CalculadoraDoAmor + "%")
